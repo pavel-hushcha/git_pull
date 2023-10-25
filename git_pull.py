afc25e4ad70db1c3ea38ed616d4c7b7904a25317
Pavel Hushcha <pavel.hushcha@gmail.com>
2023-10-25 07:41:52 +0200

    Refactored concurrent for improved output

--- git_pull.py
+++ git_pull.py
@@ -5,6 +5,7 @@ import concurrent.futures
 import pathlib
 import subprocess
 import sys
+import time
 
 
 def find_git_directories(path: pathlib.Path) -> list[str | None]:
@@ -26,7 +27,7 @@ def find_git_directories(path: pathlib.Path) -> list[str | None]:
         return git_directories
 
 
-def pull_git(directory: str) -> None:
+def pull_git(directory: str) -> tuple[str, str]:
     process = subprocess.Popen(
         ['git', 'pull'],
         stdout=subprocess.PIPE,
@@ -36,15 +37,15 @@ def pull_git(directory: str) -> None:
     )
     try:
         stdout_data, stderr_data = process.communicate(timeout=10)
-        print(f'Pulling git in {directory}')
-        print(stdout_data, stderr_data)
     except subprocess.TimeoutExpired:
         process.kill()
         stdout_data, stderr_data = process.communicate()
-        print(stdout_data, stderr_data)
+    return stdout_data, stderr_data
 
 
 def main() -> None:
+    start_time = time.time()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', action='store', type=pathlib.Path, help='The path to the folder for git pull.')
     args = parser.parse_args(sys.argv[1:])
@@ -54,11 +55,34 @@ def main() -> None:
         raise parser.error('Path must be a directory.')
     git_directories = find_git_directories(path)
     if git_directories:
+        results: dict[str, dict] = {}
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(pull_git, git_directories)
-        print(f'Processed {len(git_directories)} GIT directories. Please see the details above.')
+            futures = {}
+            for directory in git_directories:
+                future = executor.submit(pull_git, directory)
+                futures[future] = directory
+
+            for future in concurrent.futures.as_completed(futures):
+                stdout_data, stderr_data = future.result()
+                results[futures[future]] = {
+                    "Success": stdout_data,
+                    "Failed": stderr_data,
+                }
+
+        for number, directory in enumerate(results):
+            print()
+            print(f'{number + 1}. Path: {directory}')
+            print(results[directory]["Success"].rstrip())
+            if results[directory]["Failed"]:
+                print(results[directory]["Failed"].rstrip())
     else:
-        print('No GIT directory has been found.')
+        print('No GIT directories have been found.')
+
+    print('================================================================================')
+    print(f'Processed {len(git_directories)} GIT directories. Please see the details above.')
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Duration of execution: {duration:.2f} seconds")
 
 
 if __name__ == '__main__':
